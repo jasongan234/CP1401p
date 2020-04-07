9e69f5c834f11ce8f15d49173e701c584a59dd5b
Republic Of Gamers <jasongan12@gmail.com>
2020-04-07 19:33:16 +0700

    Add password check password

---
+++ password.py
@@ -0,0 +1,18 @@
+
+def main():
+    """Getting and printing password with the use of functions"""
+    minimum_length=2
+    password=get_password(minimum_length)
+    print_asterisks(password)
+
+
+def get_password(minimum_length):
+    """Get password, ensure it is above minimum lenght"""
+    password=input("Enter your password of at least {} characters: ". format(minimum_length))
+    while len(password) < minimum_length:
+        print("Password too short")
+        password = input("Enter your password of at least {} characters: ".format(minimum_length))
+    return password
+def print_asterisks(sequence):
+    print('*' * len(sequence))
+main()
